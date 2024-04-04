48cd169462f6aab14796132f5e72cbdaa1ea0fbe
AETH-erial <aetheriak@proton.me>
2024-04-03 21:02:18 -0500

    had to update a js file

--- html/htmx/json-enc.js
+++ html/htmx/json-enc.js
@@ -1,1 +1,12 @@
-Found. Redirecting to /htmx.org@1.9.10/dist/ext/json-enc.js
+htmx.defineExtension('json-enc', {
+    onEvent: function (name, evt) {
+        if (name === "htmx:configRequest") {
+            evt.detail.headers['Content-Type'] = "application/json";
+        }
+    },
+    
+    encodeParameters : function(xhr, parameters, elt) {
+        xhr.overrideMimeType('text/json');
+        return (JSON.stringify(parameters));
+    }
+});
